f2c71ee88c5f72b6d875744e1fb3b7965fece963
MHSsgiles <44165051+MHSsgiles@users.noreply.github.com>
2018-10-19 09:08:04 -0400

    Add files via upload

--- P5/Myfirstproject/sketch.js
+++ P5/Myfirstproject/sketch.js
@@ -1,9 +1,11 @@
 function setup() {
-  // put setup code here
-  createCanvas(800,600)
+createCanvas (640, 480);
 }
-
 function draw() {
-  // put drawing code here
-ellipse(200,200,50,50);
+rect(0,350,400,50);
+ellipse(200, 300, 150, 150);
+ellipse(200, 200, 100, 100);
+ellipse(200, 120, 75, 75);
+line(160,200,100,100);
+line(240,200,300,100);
 }
